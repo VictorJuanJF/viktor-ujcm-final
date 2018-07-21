2c889d04da72535066ce76c225979305064993a9
Victor Juan <vj.jimenez96@gmail.com>
2018-07-20 22:36:56 -0500

    xd

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
     FB_VERIFY_TOKEN: 'ujcmtoken',
     API_AI_CLIENT_ACCESS_TOKEN: '46f058cdd0284a8197cca92fe508b9c9',
     FB_APP_SECRET: '0913ed29ad95af132c8941f9ed46eb6b',
-    SERVER_URL: "https://ujcm-chatbot.herokuapp.com/",
+    SERVER_URL: "https://smart-ujcm.herokuapp.com/",
     SENDGRID_API_KEY: "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",
     EMAIL_FROM: 'vj.jimenez96@gmail.com',
     EMAIL_TO:'vj.jimenez96@gmail.com',
