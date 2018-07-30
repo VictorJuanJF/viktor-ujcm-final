668404daedeb1caa1fb58ebe905cc644e6c223d2
Victor Juan <vj.jimenez96@gmail.com>
2018-07-29 20:57:16 -0500

    Agregado Campo Email a users

--- user.js
+++ user.js
@@ -39,7 +39,7 @@ module.exports = {
                                     console.log('rows: ' + result.rows.length);
                                     if (result.rows.length === 0) {
                                         let sql = 'INSERT INTO users (fb_id, first_name, last_name, profile_pic, ' +
-                                            'locale, timezone, gender,email) VALUES ($1, $2, $3, $4, $5, $6, $7)';
+                                            'locale, timezone, gender) VALUES ($1, $2, $3, $4, $5, $6, $7)';
                                         console.log('sql: ' + sql);
                                         client.query(sql,
                                             [
