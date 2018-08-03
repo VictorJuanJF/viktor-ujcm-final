571e24e9f0907cb9c6f28b38193dc258ee05dc4c
Victor Juan <vj.jimenez96@gmail.com>
2018-08-03 09:06:15 -0500

    Probando Viktor UJCM Test

---
+++ config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAHGZB9V19FQBADRvAgpid7vYIg9Mh9lH0nxD9rgkZCfxUfAZCYTELMtXZCZAAcZBJfb5G6E60Un0EcYm24BNIemApuTMpWjeVXvGYZC6XDBQsyvBFoVPFffWZAI7TvnMe9y5bUuxEg5JKSN2EMvi4BprCjLudZC0tUsETaeRt6BUzJre7yWkochy',
+    FB_VERIFY_TOKEN: 'ujcmtoken',
+    API_AI_CLIENT_ACCESS_TOKEN: 'a74c5f54c76c4dda9a64cc662fbcb43a',
+    FB_APP_SECRET: 'c1f5af58f3bfec75b27c730b975e80ad',
+    FB_APP_ID:'500259897078868',
+    SERVER_URL: "https://smart-ujcm.herokuapp.com/",
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'vj.jimenez96@gmail.com',
+    ADMIN_ID:'2097337190307778',
+    EMAIL_TO:'vj.jimenez96@gmail.com',
+    PG_CONFIG: {
+        user: 'gjpwfjfsapumva',
+        database: 'd8dibo0ihah1jc',
+        password: '6eb87de21cacebf2dafc8d7e372262407164f84e6b8a0fe347be18cac3273948',
+        host: 'ec2-54-83-59-239.compute-1.amazonaws.com',
+        port: 5432,
+        max: 10,
+        idleTimeoutMillis: 30000,
+    },
+};
