1e2e066a3b324ad4702c69efb523c74312e02b90
Victor Juan <vj.jimenez96@gmail.com>
2018-08-03 22:21:01 -0500

    Agregado psid al webhook

--- routes/webviews.js
+++ routes/webviews.js
@@ -2,11 +2,19 @@
 
 const config = require('../config');
 const express = require('express');
+const fbservice = require('../fb-service/fb-service');
 
 const router = express.Router();
 
 router.get('/webview', function(req, res) {
     res.render('register-form');
-})
+});
+
+router.post('/save', function(req, res) {
+    let body = req.query;
+    let nombre = body.nombres;
+    fbservice.sendTextMessage(body.psid, `Felicidades ${nombre} ! lo lograste`);
+
+});
 
 module.exports = router;
